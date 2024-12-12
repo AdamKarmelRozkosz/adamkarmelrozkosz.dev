6a395b501694d0fa9a74c18c3a5807ca6c70aa09
Adam <65013115+AdamKarmelRozkosz@users.noreply.github.com>
2024-12-12 19:00:24 +0100

    Some changes

---
+++ script.js
@@ -0,0 +1,24 @@
+proj = document.getElementById("proj")
+cert = document.getElementById("cert")
+cd = document.getElementById("cd")
+
+proj.addEventListener("mouseover", function() {
+    cd.innerHTML= "cd projects"
+    cd.style.animation = 'typing 4s steps(41) ';
+    cd.style.width = '41ch';
+ 
+})
+proj.addEventListener("mouseleave", function() {
+    cd.innerHTML= "cd"
+    cd.style.width="0"
+})
+
+cert.addEventListener("mouseover", function() {
+    cd.innerHTML= "cd certifications"
+    cd.style.animation = 'typing 4s steps(41)';
+    cd.style.width = '41ch';   
+})
+cert.addEventListener("mouseleave", function() {
+    cd.innerHTML= "cd"
+    cd.style.width="0"
+})
